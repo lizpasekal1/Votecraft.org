a788762c4982e1980eef87bfcce55b556a1f2712
lizpasekal <32807890+lizpasekal1@users.noreply.github.com>
2026-02-02 14:26:40 -0500

    Revert "Remove src/ folder, move rcv.css to vc-widgets/rcv/"
    This reverts commit 8dd933daf2401816c3c16d7462ed55accb873f1c.

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Main Entry Point for Votecraft
+ * This file imports all modules and initializes the application
+ */
+
+// Import styles
+import '../css/styles.css';
+
+// Import API and App
+import './civic-api.js';
+import './app.js';
+
+console.log('Votecraft loaded successfully');
