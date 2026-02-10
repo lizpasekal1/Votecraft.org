34108a552d5ce27f56ed4dd89b0662c751be1a7b
lizpasekal <32807890+lizpasekal1@users.noreply.github.com>
2026-02-09 22:24:18 -0500

    Add line break in tooltip text after 'data analytics'

--- pages/vote/js/vote-app.js
+++ pages/vote/js/vote-app.js
@@ -1106,7 +1106,7 @@ class VoteApp {
                 <div class="nonprofit-card" data-np-index="${i}">
                     <div class="nonprofit-logo">${logoHtml}</div>
                     <div class="btn-donate-row">
-                        <span class="btn-donate-num">${i + 1}<span class="btn-num-tooltip"><strong><u>Email me&mdash;</u></strong>data analytics for this website for free!</span></span>
+                        <span class="btn-donate-num">${i + 1}<span class="btn-num-tooltip"><strong><u>Email me&mdash;</u></strong>data analytics<br>for this website for free!</span></span>
                         <a href="${np.donateUrl}" target="_blank" rel="noopener" class="btn-donate">${labels[i] || 'Donate'}</a>
                     </div>
                 </div>
